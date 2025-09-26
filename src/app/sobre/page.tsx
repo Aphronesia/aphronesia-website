e4e3b57eb17a70ee262681dcbe0019b7deb73efb
Ferle <frmoura15@gmail.com>
2025-09-26 09:16:31 -0300

    update 0.1.6.3

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -2,13 +2,23 @@ import Link from "next/link";
 import Header from '@/components/header'
 import Footer from '@/components/footer'
 
+import Image from 'next/image';
+
+import logo01 from '@/assets/home/logo-01.png'
 export default function Home() {
     return (
         <main>
             <Header/>
 
-            <section>
-                <h1>Sobre</h1>
+            <section className="place-items-center">
+                <div className="">
+
+                    <Image
+                        src={logo01}
+                        alt="Logo da equipe Aphronesia"
+                        width={500}
+                    />
+                </div>
             </section>
             
             <Footer />
